a7a71ca4c9988399baedec12db0e2766401e51c1
danielchtseng <danielchtseng@gmail.com>
2018-08-29 08:17:34 +0800

    add Python-Array.py

---
+++ Python-Array.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 14:53:15 2018
+
+@author: danieltseng
+"""
+
+arr = [10, 20, 30, 40, 50]
+
+# array indexing
+print(arr[0])
+print(arr[1])
+print(arr[2])
+
+# Negative Indexing
+print(arr[-1])
+print(arr[-2])
+
+
+arr = [10, 20, 30, 40, 50]
+num_arr = len(arr)
+print(num_arr)  # 5
