c3444f77d907c1ea0f89b07d679c67909de51655
danielchtseng <danielchtseng@gmail.com>
2018-08-30 06:56:56 +0800

    add NumPy-ArrayIndex.py

---
+++ NumPy-ArrayIndex.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 14:05:30 2018
+
+@author: danieltseng
+"""
+
+import numpy as np
+a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+b = a[:2, 1:3]
+print(b)
