552bbe4fe077fbcb359b4083c4415f1b4312075e
danielchtseng <danielchtseng@gmail.com>
2018-08-31 10:23:46 +0800

    del Python-Array.py add Python-List.py

---
+++ Python-List.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 23 14:53:15 2018
+
+@author: danieltseng
+"""
+
+ls1 = [10, 20, 30, 40, 50]  # square bracket
+print(len(ls1)) # 5
+print(type(ls1))
+
+# array indexing
+print(ls1[0])
+print(ls1[1])
+# Negative Indexing
+print(ls1[-1])
+print(ls1[-2])
+
+print("#####")
+ls2 = [(1,2,3),(4,5,6),(7,8,9),(10,11,12)]
+print(type(ls2))
+
+print(ls2)
+print(ls2[0])
+print(ls2[-1])
