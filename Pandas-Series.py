87f7b1be91eb1cbabcd63b90e7bb58ea6ee17604
danielchtseng <danielchtseng@gmail.com>
2018-08-31 09:30:49 +0800

    modify Pandas-Series.py

--- Pandas-Series.py
+++ Pandas-Series.py
@@ -10,19 +10,16 @@ import pandas as pd
 
 # 資料為 Array
 
-cars = ["BMW", "BENZ", "Toyota", "Nissan", "Lexus"]
+cars = ["BMW", "BENZ", "Toyota", "Nissan", "Lexus"] # square kracket
 
 select = pd.Series(cars)  
 print(select)  
+print (type (select))
 print("#####")  
 
 
-
 #資料為 Dictionary
-
-import pandas as pd
-
-dict = {  
+dict = {    # curly bracket
     "factory": "Taipei",
     "sensor1": "1",
     "sensor2": "2",
@@ -31,11 +28,14 @@ dict = {
     "sensor5": "5"
 }
 
+
+print("=====")  
 select = pd.Series(dict, index = dict.keys()) # 排序與原 dict 相同  
+print (type (select))
 print(select[0])  
-print("=====")  
+
 print(select['sensor1'])  
-print("=====")  
+
 print(select[[0, 2, 4]])  
-print("=====")  
+
 print(select[['factory', 'sensor1', 'sensor3']]) 
